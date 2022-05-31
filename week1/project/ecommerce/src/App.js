282de64c8e425f5edbe78ab94fe803b976dc5e8c
Mo Sabouni <M_95_@hotmail.com>
2022-05-31 17:58:02 +0200

    Deleted unused files and import

--- week1/project/ecommerce/src/App.js
+++ week1/project/ecommerce/src/App.js
@@ -2,16 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import CategoryList from "./components/CategoryList";
 import ProductsList from "./components/ProductsList";
-import productsData from "./fake-data/all-products";
-import categoriesData from "./fake-data/all-categories";
 
 function App() {
   const [category, setCategory] = useState("all");
-  const [activeButton, setActiveButton] = useState({ activeButton: null });
+  const [activeButton, setActiveButton] = useState(null);
 
   return (
     <div className="App">
-      <h1>Products</h1>
+      <h1 className="header">Products</h1>
       <CategoryList
         setCategory={setCategory}
         category={category}
--- week1/project/ecommerce/src/components/CategoryItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CategoryItem() {
-  return <div>catagoryItem</div>;
-}
-
-export default CategoryItem;
